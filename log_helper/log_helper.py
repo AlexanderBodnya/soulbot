568b204a3528b13a963c04ed6be50fd42f360a7a
Alexander Bodnya <bodnya.aa@gmail.com>
2018-03-02 08:06:05 +0000

    Formatted with accordance to PEP8 standart, comment lines move as per request

--- log_helper/log_helper.py
+++ log_helper/log_helper.py
@@ -3,60 +3,51 @@ import logging.handlers
 import time
 
 
-
 class LogHelper(logging.Logger):
-    
+    # Redefining logging levels for convinience purposes
     levels = {
-        0 : logging.CRITICAL,
-        1 : logging.ERROR,
-        2 : logging.WARNING,
-        3 : logging.INFO,
-        4 : logging.DEBUG
-        }
-    # Redefining logging levels for convinience purposes 
+        0: logging.CRITICAL,
+        1: logging.ERROR,
+        2: logging.WARNING,
+        3: logging.INFO,
+        4: logging.DEBUG
+    }
 
+    # Instance initialization function takes logger name as argument and provides instance of logger with defined format parameters
     def __init__(self, logger_name):
         self.logger = logging.getLogger(logger_name)
-        
-        self.formatter = logging.Formatter('%(processName)s %(process)d %(levelname)s %(asctime)s %(message)s', 
+        self.formatter = logging.Formatter(
+            '%(processName)s %(process)d %(levelname)s %(asctime)s %(message)s',
             datefmt='%m/%d/%Y %I:%M:%S %p'
             )
-    # Instance initialization function takes logger name as argument and provides instance of logger with defined format parameters
-    
+
+    # Redefined logging.log function, which allows to take logging levels as int numbers
     def log_message(self, level, *args):
         self.logger.log(self.levels[level], *args)
-        
-    # Redefined logging.log function, which allows to take logging levels as int numbers    
 
-    def set_level(self,log_level):
-        self.logger.setLevel(self.levels[log_level])
-        
     # Set log level of the logger instance
-        
+    def set_level(self, log_level):
+        self.logger.setLevel(self.levels[log_level])
+
+    # Create handler to send logs to the file
     def file_handler(self):
         filename = time.strftime("%Y%m%d-%H%M%S")+'_soulbot.log'
         handler = logging.FileHandler(filename)
         handler.setFormatter(self.formatter)
         return handler
-    
-    # Create handler to send logs to the file
-    
+
+    # Create handler to send logs to the stdout
     def stream_handler(self):
         handler = logging.StreamHandler()
         handler.setFormatter(self.formatter)
         return handler
-    
-    # Create handler to send logs to the stdout
-        
+
+    # Attach handler tot the logger instance
     def attach_handler(self, handler):
         self.logger.addHandler(handler)
-        
-    # Attach handler tot the logger instance
 
 
-
-#some examples       
-        
+# some examples
 log = LogHelper('test_log')
 log.set_level(3)
 sh = log.stream_handler()
