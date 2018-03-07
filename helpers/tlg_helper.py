e9e694bd126d57334a4009b283477d0c312fb54b
bodnya.aa <bodnya.aa@gmail.com>
2018-03-07 14:50:39 +0300

    new api methods

--- helpers/tlg_helper.py
+++ helpers/tlg_helper.py
@@ -17,11 +17,21 @@ class BotHelper:
         else:
             return 1
 
-    def setWebhook(self, wh_url, cert):
-        payload = {
-            'url': wh_url,
-            'cert': open(cert, 'r')
-        }
+    def setWebhook(self, url=None, cert=None, max_connections=None, allowed_updates=None, **kwargs):
+        payload = {}
+
+        if url is not None:
+            payload['url'] = url
+        if cert is not None:
+            payload['certificate'] = open(cert, 'rb')
+        if max_connections is not None:
+            payload['max_connections'] = max_connections
+        if allowed_updates is not None:
+            payload['allowed_updates'] = allowed_updates
+        payload.update(kwargs)
+
+
+        print(payload)
         result = self.api_request('setWebhook', payload)
         return result
 
@@ -36,5 +46,3 @@ class BotHelper:
     def getWebhookInfo(self):
         result = self.api_request('getWebhookInfo')
         return result
-
-
