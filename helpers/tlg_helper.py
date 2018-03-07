f2bb91aa4c171d878435f8fd35d0d9c4d0f8d3d0
bodnya.aa <bodnya.aa@gmail.com>
2018-03-07 21:21:44 +0300

    commentaries added

--- helpers/tlg_helper.py
+++ helpers/tlg_helper.py
@@ -2,16 +2,21 @@ import requests
 
 
 class BotHelper:
-
+    # Here we are initializang instance of our bot. It does nothing except stores bot token
     def __init__(self, token):
         self.token = token
 
+    # This is serialized api request. It is separated from api methods because DNRY(do not repeat yourself)
     def api_request(self, method_name, payload={}):
         url = 'https://api.telegram.org/bot' + self.token + '/' + method_name
+        # Here we are separating setWebhook method from other methods.
+        # setWebhook requires multipart/form-data,
+        # requests lib requires to use special parameter files to use the format
         if method_name == 'setWebhook':
             req = requests.post(url, files=payload)
         else:
             req = requests.post(url, data=payload)
+        # Here we are trying to handle possible exceptions. It needs to be redesigned, however it works at the moment
         try:
             resp = req.json()
             return 0, resp
@@ -20,11 +25,14 @@ class BotHelper:
         else:
             return 1
 
+    # This is API method to set webhook. It doesn't have required options, however if you want it to work
+    # you need to specify url(this is url on which Telegram will send messages), and path to cert file,
+    # in case you are using self-signed certificate
     def setWebhook(self, url=None, cert=None, max_connections=None, allowed_updates=None, **kwargs):
         payload = {
             'url': (None, url)
         }
-
+        # We are checking if parameters are passed and adding them to request
         if cert is not None:
             payload['certificate'] = (cert, open(cert, 'rb'))
         if max_connections is not None:
@@ -36,14 +44,17 @@ class BotHelper:
         result = self.api_request('setWebhook', payload)
         return result
 
+    # This is API method which deletes webhook. It doesn't require any parameters.
     def deleteWebhook(self):
         result = self.api_request('deleteWebhook')
         return result
 
+    # This is API method which returns information about bot. It doesn't require any parameters
     def getMe(self):
         result = self.api_request('getMe')
         return result
 
+    # This is API method which returns information about webhook. It doesn't require any parameters
     def getWebhookInfo(self):
         result = self.api_request('getWebhookInfo')
         return result
