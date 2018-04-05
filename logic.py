fd2f49ca7a3797b5d1b41fe2c238d1db4fc84558
bodnya.aa <bodnya.aa@gmail.com>
2018-04-05 13:12:43 +0300

    RMQ helper reworked into class QueueHelper; added checking functionality, if the connection is closed, the exception is caught and new connection is opened; same functionality added for message consuming

--- logic.py
+++ logic.py
@@ -1,10 +1,6 @@
-#!/usr/bin/env python
-
 import helpers.rmq_helper as q_helper
 import configs.config as conf
 import helpers.tlg_helper as tlg
-import json
-
 
 # TODO: log it!
 bot = tlg.BotHelper(conf.TOKEN)
@@ -15,17 +11,12 @@ def on_message(ch, method, properties, body):
     msg = tlg.Messaging(conf.TOKEN, body)
     if msg.get_command() is not None:
         msg.command_execute(msg.get_command())
-
+        return 0
     result = bot.sendMessage(msg.get_chat_id(), msg.get_text())
     return result
 
 
-queue = 'bot_inbox'
-
 if __name__ == '__main__':
-    queue_channel = q_helper.start_queue(queue)
-    # TODO: implement message acknowledgment
-    queue_channel.basic_consume(on_message,
-                                queue=queue,
-                                no_ack=True)
-    queue_channel.start_consuming()
+    queue_handler = q_helper.QueueHelper(conf.QUEUE_NAME)
+    queue_handler.consume_message(on_message)
+    queue_handler._channel.start_consuming()
