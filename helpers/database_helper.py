6fc1bd5d1b03c7aa6189db8e57e62b341a4949fb
bodnya.aa <bodnya.aa@gmail.com>
2018-06-19 13:42:52 +0300

    added database initialization, table creation, a few insertion functions

--- helpers/database_helper.py
+++ helpers/database_helper.py
@@ -1,11 +1,16 @@
 import sqlite3
 
+def connect(database):
+    database = sqlite3.connect(conf.DATABASE)
+    cur = database.cursor()
+    init_db(cur)
+    return cur, database
 
 def init_db(cur):
     cur.execute(
         '''CREATE TABLE IF NOT EXISTS users (user_id INTEGER,
                                              chat_id INTEGER,
-                                             pair_id INTEGER, 
+                                             pair_id INTEGER,
                                              name TEXT,
                                              nickname TEXT,
                                              pair_name TEXT,
@@ -25,3 +30,13 @@ def init_db(cur):
                                                FOREIGN KEY (from_user_id) REFERENCES users(user_id),
                                                FOREIGN KEY (to_user_id) REFERENCES users(user_id),
                                                FOREIGN KEY (pair_id) REFERENCES users(pair_id))''')
+    cur.execute("CREATE INDEX IF NOT EXISTS `IX_fuid` ON `history` ( `from_user_id` )")
+    cur.execute("CREATE INDEX IF NOT EXISTS `IX_tuid` ON `history` ( `to_user_id` )")
+    cur.execute("CREATE INDEX IF NOT EXISTS `IX_pid` ON `history` ( `pair_id` )")
+
+
+def store_message(cur, from_user_id, message):
+    cur.execute('INSERT INTO history (from_user_id, message) VALUES(?,?)',(from_user_id, message))
+
+def add_user(cur, user_id, chat_id, nickname):
+    cur.execute('INSERT INTO users (user_id, chat_id, nickname) VALUES(?,?,?)',(user_id, chat_id, nickname))
--- logic.py
+++ logic.py
@@ -1,19 +1,22 @@
 import helpers.rmq_helper as q_helper
 import configs.config as conf
 import helpers.tlg_helper as tlg
+import helpers.database_helper as db
+
 
 # TODO: log it!
 bot = tlg.BotHelper(conf.TOKEN)
-
+cur, database = db.connect(conf.DATABASE)
 
 def on_message(ch, method, properties, body):
     print(" [x] Received %r" % body)
     msg = tlg.Messaging(conf.TOKEN, body)
     if msg.get_command() is not None:
-        print(msg.get_command())
         msg.command_execute(msg.get_command())
         return 0
     result = bot.sendMessage(msg._chat_id, msg.get_text())
+    db.store_message(cur, msg.get_user_id(), msg.get_text())
+    database.commit()
     return result
 
 
